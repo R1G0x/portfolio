c9ee38c7d6cff59dc6ea9dcbffd61e7da3ead02b
Rigo Reatiga <rigo.reatiga@gmail.com>
2018-04-26 21:26:33 +0000

    Implemented destroy action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -33,6 +33,13 @@ class PortfoliosController < ApplicationController
         end
     end
     
+    def destroy
+        @portfolio_item.destroy
+        respond_to do |format|
+            format.html { redirect_to portfolios_url, notice: 'Portfolio was removed.' }
+        end
+    end
+    
     private
     
     def portfolio_params
