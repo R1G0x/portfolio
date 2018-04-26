a7e6e081ec9c1225708997b961394eda2a3c4321
Rigo Reatiga <rigo.reatiga@gmail.com>
2018-04-26 21:43:09 +0000

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   resources :portfolios
   resources :blogs
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
 end
