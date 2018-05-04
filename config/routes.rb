bda7ad1f5eee19955198ee9ecd44fde35175a6dc
Rigo Reatiga <rigo.reatiga@gmail.com>
2018-05-04 15:21:03 +0000

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :portfolios
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  resources :portfolios, except: [:show]
   resources :blogs
 end
